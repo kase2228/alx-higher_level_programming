395a17ac4d52034dfc56f661cc5ecbfbcd6751d7
etel <etelethiopia@gmail.com>
2024-01-10 09:51:42 -0800

    task 13

---
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -0,0 +1,12 @@
+def weight_average(my_list=[]):
+    if not my_list:
+        return 0
+
+    numerator = 0
+    denominator = 0
+
+    for score, weight in my_list:
+        numerator += score * weight
+        denominator += weight
+
+    return numerator / denominator
