df6ea8edda1663a3e103a299df5f51c0d59210fc
etel <etelethiopia@gmail.com>
2024-01-10 08:40:35 -0800

    task 2

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
-def add_unique_elements(my_list):
-    unique_elements = set(my_list)
+def uniq_add(my_list=[]):
+    unique_elements = []
+    for item in my_list:
+        if item not in unique_elements:
+            unique_elements.append(item)
     return sum(unique_elements)
