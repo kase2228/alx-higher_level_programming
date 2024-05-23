e5f69234e61b3cbea210b394d39c30f221aa4453
kase2228 <etelethiopia@gmail.com>
2024-05-23 13:53:10 -0700

    task 11

--- 0x0D-SQL_introduction/11-best_score.sql
+++ 0x0D-SQL_introduction/11-best_score.sql
@@ -1,2 +1,2 @@
 -- retrives name and score of those who score above or equal to 10
-SELECT score, name FROM second_table WHERE id>=10 ORDER BY score DESC;
+SELECT score, name FROM second_table WHERE score>=10 ORDER BY score DESC;
